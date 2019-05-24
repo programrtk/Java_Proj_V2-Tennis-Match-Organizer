9dd25e74f313f2d4c6364dfd371155daf225b022
guptas5279 <guptas5279@CA-SU-F106-11.sduhsd.lan>
2019-05-24 08:11:12 -0700

    progress

---
+++ src/TennisMatchOrganizer/Match.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TennisMatchOrganizer;
+
+/**
+ *
+ * @author guptas5279
+ */
+public class Match {
+    
+    private int player1Id;
+    private int player2Id;
+    private String location;
+    private String date;
+    private String time;
+    private int winnerId;
+    
+    
+           
+    
+}
