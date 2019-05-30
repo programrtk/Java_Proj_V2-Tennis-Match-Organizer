062afe35b65a17d3a8d91b539fdef1f0b9631387
SumitGaurav <SumitGaurav@Lenovo-PC.sduhsd.lan>
2019-05-30 11:55:26 -0700

    Frame added

--- src/TennisMatchOrganizer/ExampleFrame.java
+++ src/TennisMatchOrganizer/ExampleFrame.java
@@ -9,12 +9,12 @@ package TennisMatchOrganizer;
  *
  * @author guptas5279
  */
-public class TennisAppFrame extends javax.swing.JFrame {
+public class ExampleFrame extends javax.swing.JFrame {
 
     /**
      * Creates new form TennisAppFrame
      */
-    public TennisAppFrame() {
+    public ExampleFrame() {
         initComponents();
     }
 
@@ -123,20 +123,21 @@ public class TennisAppFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TennisAppFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ExampleFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TennisAppFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ExampleFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TennisAppFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ExampleFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TennisAppFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ExampleFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new TennisAppFrame().setVisible(true);
+                new ExampleFrame().setVisible(true);
             }
         });
     }
